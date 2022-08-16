0e1cea3acfd37c71bd6aaf53d5473ca0ceafc57c
Shulaev <mail@ntdservice.ru>
2022-08-17 01:20:58 +0300

    fix_menu

--- task.js
+++ task.js
@@ -3,22 +3,23 @@
 window.addEventListener('DOMContentLoaded', ()=> {
 
     let subMenu = Array.from(document.querySelectorAll('.menu_sub'))
-    
 
     subMenu.forEach((item)=>{
 
         let link = item.closest('.menu__item').querySelector('.menu__link')
 
         link.addEventListener('click', (e)=>{
-            e.preventDefault()
 
+            e.preventDefault()
             let nextMenu = link.nextElementSibling
 
             if  (nextMenu && !nextMenu.classList.contains('menu_active')) {
+
                 hideMenu()
                 nextMenu.classList.add('menu_active')
                 return false
             }else {
+
                 hideMenu()
                 nextMenu.classList.remove('menu_active')
                 return false
@@ -30,7 +31,9 @@ window.addEventListener('DOMContentLoaded', ()=> {
         let subMenu = Array.from(document.querySelectorAll('.menu_sub'))
 
         subMenu.forEach((item)=>{
+
             if(item && item.classList.contains('menu_active')) {
+                
                 item.classList.remove('menu_active')
             };
         });
